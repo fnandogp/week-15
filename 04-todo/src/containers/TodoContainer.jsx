681aa2db3761dea38e5a7503edda5693d609005c
Fernando Pinheiro <fnandogp@gmail.com>
2022-04-08 17:57:48 +1000

    add styling to todo list

--- 04-todo/src/containers/TodoContainer.jsx
+++ 04-todo/src/containers/TodoContainer.jsx
@@ -1,43 +1,45 @@
-import { useState } from 'react'
-import Header from '../components/Header'
-import TodoList from '../components/TodoList'
-import AddTodo from '../components/AddTodo'
-import TodoSummary from '../components/TodoSummary'
+import { useState } from "react";
+import Header from "../components/Header";
+import TodoList from "../components/TodoList";
+import AddTodo from "../components/AddTodo";
+import TodoSummary from "../components/TodoSummary";
 
 export default function TodoContainer() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (description) => {
     const newTodo = {
       id: new Date().getTime(),
       description: description,
       completed: false,
-    }
-    setTodos([...todos, newTodo])
-  }
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   const toggleCompleted = (id) => {
     const newTodos = todos.map((todo) => {
-      if (todo.id !== id) return todo
+      if (todo.id !== id) return todo;
 
-      return { ...todo, completed: !todo.complete }
-    })
+      return { ...todo, completed: !todo.completed };
+    });
 
-    setTodos(newTodos)
-  }
+    setTodos(newTodos);
+  };
 
   return (
     <>
       <Header />
       <main className="container mx-auto p-4">
-        <section className="">
-          <TodoList todos={todos} onClick={toggleCompleted} />
-          <AddTodo onSubmit={addTodo} />
-        </section>
-        <section className="">
-          <TodoSummary todos={todos} />
-        </section>
+        <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-4">
+          <section className="md:col-span-3 col-auto">
+            <TodoList todos={todos} onClick={toggleCompleted} />
+            <AddTodo onSubmit={addTodo} />
+          </section>
+          <section className="md:col-span-1 col-auto">
+            <TodoSummary todos={todos} />
+          </section>
+        </div>
       </main>
     </>
-  )
+  );
 }
